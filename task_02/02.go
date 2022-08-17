b292b7cae91b335b2a141c28ffca24809e83423e
Lenulya <vivod@bk.ru>
2022-08-17 10:50:13 +0300

    finish 02

--- task_02/02.go
+++ task_02/02.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"sort"
-	"strings"
+"bufio"
+"fmt"
+"os"
+"sort"
+"strings"
 )
 
 func main(){
@@ -14,21 +14,25 @@ func main(){
 	var ind_str string
 
 	fmt.Scan(&N)
+  
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+	if sc.Text() == "" {
+		break
+	}
+	a := sc.Text()
+	a = strings.Trim(a, "\n")
+	b := strings.Split(a, " ")
+	sort.Strings(b)
+	for _, v := range b{
+	ind_str += v + " "
+	}
 
-	for i := 0; i < N; i++{
-		a, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		a = strings.Trim(a, "\n")
-		b := strings.Split(a, " ")
-		sort.Strings(b)
-		for _, v := range b{
-			ind_str += v + " "
-		}
-
-		if _, ok := res[ind_str]; !ok{
-			res[ind_str] = 0
-		} else {
-			res[ind_str]++
-		}
+	if _, ok := res[ind_str]; !ok{
+		res[ind_str] = 1
+	} else {
+		res[ind_str]++
+	}
 		ind_str = ""
 	}
 	var value []int
@@ -38,11 +42,3 @@ func main(){
 	sort.Ints(value)
 	fmt.Println(value[len(value)-1])
 }
-
-//test
-// 5
-// MIKHAIL VLADISLAV GRIGORY
-// VLADISLAV MIKHAIL GRIGORY
-// IVAN ILYA VLADIMIR
-// ANDREY VLADIMIR ILYA
-// VLADIMIR IVAN ANDREY
